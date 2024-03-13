7dfb7bda5923dfc7b8d63f323dc77848818fc7ff
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-13 14:01:23 -0400

    Add logic for locateUserButton

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -2,5 +2,36 @@ class PlaceFinder {
     constructor() {
         const addressForm = document.querySelector("form");
         const locateUserButton = document.getElementById("locate-btn");
+
+        locateUserButton.addEventListener("click", this.locateUserHandler);
+        addressForm.addEventListener("submit", this.findAddressHandler);
     }
+
+    // handles user trying to get current location
+    locateUserButton(event) {
+        if (!navigator.geolocation) {
+            alert(
+                "Location feature is not available in your browser. Please use a modern browser."
+            );
+            return;
+        }
+
+        navigator.geolocation.getCurrentPosition(
+            (successResult) => {
+                const coordinates = {
+                    lat: successResult.lat,
+                    long: successResult.long,
+                };
+            },
+            (error) => {
+                alert("Failed to get current location due to error.");
+                return;
+            }
+        );
+    }
+
+    // handles user trying to get location by manually entering address (string)
+    findAddressHandler(event) {}
 }
+
+new PlaceFinder();
